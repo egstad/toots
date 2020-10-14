243a68180c696b127da85d25891ff0d50b0f6bb3
Egstad <jordanegstad@gmail.com>
2020-10-13 19:43:39 -0700

    now able to select multiple elements

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// query your elements here...
+// elements
 const elToggle = document.querySelector('.toot-toggle');
-const elText = document.querySelector('p');
+const elsText = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, li');
 const elToot = document.querySelector('audio');
 
-
 // locates a string and replaces it with another
-const findAndReplace = function(find, replace) {
+const findAndReplace = function(el, find, replace) {
   // from
-  let string = elText.innerText;
+  let string = el.innerText;
   // to
   let replacedString = string.replaceAll(find, replace);
   // update the dom
-  elText.innerText = replacedString;
+  el.innerText = replacedString;
+}
+
+// 
+const updateAllText = function() {
+  elsText.forEach(function(textElement) {
+    findAndReplace(textElement, 'Donald Trump', 'Toots');
+  })
 }
 
 // click event 
 const trumpToToots = function() {
-  findAndReplace('Donald Trump', 'Toots');
+  updateAllText()
   elToot.play();
 }
 
